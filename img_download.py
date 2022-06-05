e9dfdbb1309b9e1f9a4c78cf56ec96c1cc6c7900
Goldenbeasty <goldenbeaster@gmail.com>
2022-06-05 17:24:43 +0300

    Added icon downloading

---
+++ img_download.py
@@ -0,0 +1,52 @@
+import requests
+import os
+from multiprocessing.pool import ThreadPool
+import configparser
+import json
+
+config_data = configparser.ConfigParser(interpolation=None)
+config_data.read('config.ini')
+
+host = config_data['host']['hostname'] + '_'
+
+usrcount = config_data['host']['usercount']
+# open the file hostname_userid and read and import 
+failedlist = []
+
+def getfile(config, uid):
+    with open(os.path.dirname(os.path.abspath(__file__)) + '/users/' + host + str(uid), 'r') as f:
+        info = json.loads(f.read())
+        f.close()
+    return(info)
+
+def download_image_and_save(indata):
+    id, link = indata
+    r = requests.get(link)
+    if r.status_code != 200:
+        failedlist.append(link)
+    with open(os.path.dirname(os.path.abspath(__file__)) + '/icons/' + host + str(id) + '.png', 'wb') as f:
+        f.write(r.content)
+        f.close()
+    return(id)
+
+def downloadicons(config):
+    threadcount = int(config['system']['threadcount'])
+    list_of_links = []
+    # get the avatar max from
+    for user in range(int(usrcount)):
+        try:
+            user = user + 1
+            userdata = getfile(config, user)
+            link = userdata['avatar']['max']
+            list_of_links.append([user, link])
+        except KeyError:
+            continue
+
+    response = ThreadPool(threadcount).imap_unordered(download_image_and_save, list_of_links)
+    for res in response:
+        print(res)
+
+
+    
+if __name__ == '__main__':
+    downloadicons(config_data)
